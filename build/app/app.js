38a2a10a4595257ce9021a07cd8f9e443b428381
simplesthing <i.am.simplesthing@gmail.com>
2015-10-06 20:42:10 -0700

    init

---
+++ build/app/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function (window, angular, undefined) {
+
+  angular.module('roman-numerals', ['ui.router', 'Ramda']).value('_', window.lodash).config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    // For any unmatched url, redirect to /overview
+    // when there is an empty route, redirect to /index
+    $urlRouterProvider.when('', '/home');
+
+    $stateProvider.state('home', {
+      url: '/home',
+      views: {
+        'main': {
+          templateUrl: 'src/app/roman-numeral/roman-numeral.html'
+        }
+      }
+    });
+  }]);
+})(window, window.angular);
---
+++ build/app/roman-numeral/roman-numeral-controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function (window, angular, undefined) {
+	function RomanNumeralController(RNS) {
+		var model = this;
+
+		model.convert = function (number) {
+			model.rn = RNS.integerToRomanNumeral(number);
+		};
+	}
+
+	RomanNumeralController.$inject = ['RomanNumeralService'];
+
+	angular.module('roman-numerals').controller('RomanNumeralController', RomanNumeralController);
+})(window, window.angular);
